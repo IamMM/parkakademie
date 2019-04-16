3358e935c941bf8178228a07796767bd4f90c79d
Maximilian Maske <maximilian.maske@gmail.com>
2019-04-16 20:02:12 +0200

    correct single-el_event.php

--- themes/Parkakademie/single-el_events.php
+++ themes/Parkakademie/single-el_events.php
@@ -4,20 +4,30 @@
 
 <div class="container">
 
-  <?php if(has_post_thumbnail()) : ?>
+  <?php if (have_posts()) :
+  while ( have_posts() ) : the_post(); ?>
+
   <!-- The Featured Image -->
+  <?php if(has_post_thumbnail()) : ?>
   <div class="wp-block-cover" style="background-image:url( <?php the_post_thumbnail_url('full'); ?>)"></div>
   <?php endif; ?>  
-  
+
   <!-- The Content -->
   <div class="mt-3">
-    <h2 class="text-center"><?php echo get_the_title( get_the_ID() );?></h2>
-    <?php 
-    $post_object = get_post( get_the_ID() );
-    echo $post_object->post_content;
-    ?>
+    <div class="text-center">
+      <h2><?php echo get_the_title();?></h2>
+      <h3>
+        <strong><?php print get_post_meta($event_id, 'starttime', true); ?></strong>
+        <span><?php print get_post_meta($event_id, 'location', true); ?></span>
+      </h3>
+    </div>
 
-    <p>Das ist die Event Singel Page</p>
+    <div class="text-justify mt-3 mb-5">
+      <?php the_content(); ?>
+    </div>
+    <?php endwhile;
+    endif; ?>
+
   </div>
 </div>
 
