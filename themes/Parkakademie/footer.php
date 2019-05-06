3f571e96b1f8747ee20a4240c2abe9a456ebe538
Maximilian Maske <maximilian.maske@gmail.com>
2019-05-06 18:05:44 +0200

    local src

--- themes/Parkakademie/footer.php
+++ themes/Parkakademie/footer.php
@@ -22,16 +22,15 @@
 
 <!-- Bootstrap with jQuery and popper.js
 ================================================== -->
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery-3.4.1.min.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/popper.min.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/tether.min.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/bootstrap.min.js"></script>
 
 
 <!-- Swiper
 ================================================== -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/swiper.min.js"></script>
 
 
 <!-- Custom script
