ba5774ac5acc134c5924b534ddfcafc1893aa77c
Maximilian Maske <maximilian.maske@gmail.com>
2019-07-12 17:58:38 +0200

    bug fix

--- themes/Parkakademie/index.php
+++ themes/Parkakademie/index.php
@@ -1,8 +1,9 @@
 <?php get_header();?>
 
+<div id="main-container" class ="container">
+
 <?php if(have_posts()) : ?>
 <?php while(have_posts()) : the_post(); ?>
-<div id="main-container" class ="container">
   <h1 class="text-center"><?php the_title(); ?></h1>
   <hr>
   <?php the_content(); ?>
@@ -10,6 +11,7 @@
   <?php else : ?>
   <p><?php__('Keinen Beitrag gefunden'); ?></p>
   <?php endif; ?>
+  
 </div>
 
 <?php get_footer();?>
